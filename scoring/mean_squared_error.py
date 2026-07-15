98224d5dac893c334171835ecb53c63dc03ff1d9
Nicolai Müller <nicolaimue@gmail.com>
2026-07-15 09:54:39 +0200

    Fix shape mismatch error

--- scoring/mean_squared_error.py
+++ scoring/mean_squared_error.py
@@ -8,4 +8,12 @@ def mean_squared_error(
     pred_y: np.ndarray,
     true_y: np.ndarray
 ) -> floating[Any]:
+    num_samples, num_targets = true_y.shape[0], true_y.shape[1]
+
+    if pred_y.shape[0] != num_samples:
+        raise RuntimeError("Shape mismatch between predictions and ground truth.")
+
+    if pred_y.ndim == 1:
+        pred_y = pred_y.reshape((num_samples, num_targets))
+
     return np.mean((pred_y - true_y) ** 2)
